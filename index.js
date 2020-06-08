89824ab791b0e0423736d688cee16afdd463d8f1
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-07 22:46:31 -0400

    Refactor database connection setup

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
 const express = require('express');
 const server = express();
-const config = require('./config/dev')
-
-const mongoose = require('mongoose');
-mongoose.connect(config.DB_URI,
-    {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-    },
-    (err) => {
-        if (err) {
-            console.error(err);
-        } else {
-        console.log('Connected to DB!');
-        }
-    }
-)
+const {connect} = require('./db');
 
 const portfolioRoutes = require('./routes/portfolios')
 
 // server.get('/test', (req, res) => {     return res.json({message: 'test is
 // working!'}); })
 
-server.use('/api/v1/portfolios', portfolioRoutes);
+async function runServer() {
+    
+   await connect();
+   server.use('/api/v1/portfolios', portfolioRoutes);
+   
+   const PORT = parseInt(process.env.PORT, 10) || 3001;
+   server.listen(PORT, (err) => {
+       if (err) 
+           console.error(err);
+       console.log('Server ready on port ', PORT);
+   })
+}
 
-const PORT = parseInt(process.env.PORT, 10) || 3001;
-server.listen(PORT, (err) => {
-    if (err) 
-        console.error(err);
-    console.log('Server ready on port ', PORT);
-})
+runServer();
+
+
