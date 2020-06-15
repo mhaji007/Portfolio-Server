f2418ca59c2e5ef9c7c091e137700e4d240a5d46
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-06-15 18:41:54 -0400

    Update portfolio on server

--- controllers/portfolios.js
+++ controllers/portfolios.js
@@ -3,10 +3,8 @@
 const mongoose = require('mongoose');
 const Portfolio = mongoose.model('Portfolio');
 
-
-// exports.getPortfolios = (req, res) => {
-//     return res.json({data: [1,22,3]});
-// }
+// exports.getPortfolios = (req, res) => {     return res.json({data:
+// [1,22,3]}); }
 
 exports.getPortfolios = async (req, res) => {
     const portfolios = await Portfolio.find({});
@@ -15,28 +13,53 @@ exports.getPortfolios = async (req, res) => {
 
 exports.getPortfolioById = async (req, res) => {
     try {
-    const portfolio = await Portfolio.findById(req.params.id);
-    return res.json(portfolio);
-    } catch(error) {
-        return res.status(422).send(error.message);
+        const portfolio = await Portfolio.findById(req.params.id);
+        return res.json(portfolio);
+    } catch (error) {
+        return res
+            .status(422)
+            .send(error.message);
     }
 }
-
 
 exports.createPortfolio = async (req, res) => {
     const portfolioData = req.body;
     //TODO: Extract from req
-    const userId = 'auth0|5ed95170a36eb2001976617a';
+    // CheckJWT will provide user on a req object in the next middleware
+    const userId = req.user.sub;
     // Make an instance of the Portfolio model using portfolio data
     const portfolio = new Portfolio(portfolioData);
-    
+
     portfolio.userId = userId;
-    
+
     try {
         const newPortfolio = await portfolio.save();
         return res.json(newPortfolio);
-    } catch(error) {
-        return res.status(422).send(error.message);
+    } catch (error) {
+        return res
+            .status(422)
+            .send(error.message);
     }
-  
+
+}
+exports.updatePortfolio = async (req, res) => {
+    const {body, params: {
+            id
+        }} = req;
+
+    try {
+        const updatedPortfolio = await Portfolio.findOneAndUpdate({
+            _id: id
+        }, body, {
+            // Make sure we will always get the updated portfolio back
+            new: true,
+            // Make sure validators of the MongoDB are run
+            runValidators: true
+        })
+        return res.json(updatedPortfolio);
+    } catch (error) {
+        return res
+            .status(422)
+            .send(error.message);
+    }
 }
--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,15 +1,21 @@
+
 // Endpoint responsible for handling request from the browser
 // Forwards requests to controllers/portfolios.js
 
 const express = require('express');
 const router = express.Router();
 const {checkJwt} = require('../controllers/auth');
-const {getPortfolios, getPortfolioById, createPortfolio} = require('../controllers/portfolios');
-
+const {getPortfolios,
+     getPortfolioById,
+      createPortfolio,
+      updatePortfolio } = require('../controllers/portfolios');
 
 router.get('', getPortfolios);
 router.get('/:id', getPortfolioById);
 
+//TODO: create middleware to check for admin rights
 router.post('', checkJwt, createPortfolio)
+
+router.patch('/:id', updatePortfolio);
 
 module.exports = router;
